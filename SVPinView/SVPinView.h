17662063b3433e806465793aa69ccd66ee97fcba
Srinivas Vemuri <srinivas@mobcast.in>
2018-02-27 14:32:47 +0530

    Initial Commit

---
+++ SVPinView/SVPinView.h
@@ -0,0 +1,19 @@
+//
+//  SVPinView.h
+//  SVPinView
+//
+//  Created by Srinivas on 27/02/18.
+//  Copyright © 2018 Srinivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVPinView.
+FOUNDATION_EXPORT double SVPinViewVersionNumber;
+
+//! Project version string for SVPinView.
+FOUNDATION_EXPORT const unsigned char SVPinViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVPinView/PublicHeader.h>
+
+
